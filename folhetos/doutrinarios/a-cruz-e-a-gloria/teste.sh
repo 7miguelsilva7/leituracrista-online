f32939bae691961132bbbaf42017ad97ef222a79
7miguelsilva7 <7miguelsilva7@gmail.com>
2017-08-16 00:21:31 -0300

    update

---
+++ folhetos/doutrinarios/a-cruz-e-a-gloria/teste.sh
@@ -0,0 +1,11 @@
+sed -i "s/<a href\=\"http\:\/\/bibliaonline/<a data-toggle\=\"modal\" href\=\"\#myModal\" onclick\=\'loadURL\(\"https\:\/\/www.bibliaonline/g" index.html
+sed -i "s/1\">/1\"\)\'>/g" index.html
+sed -i "s/2\">/2\"\)\'>/g" index.html
+sed -i "s/3\">/3\"\)\'>/g" index.html
+sed -i "s/4\">/4\"\)\'>/g" index.html
+sed -i "s/5\">/5\"\)\'>/g" index.html
+sed -i "s/6\">/6\"\)\'>/g" index.html
+sed -i "s/7\">/7\"\)\'>/g" index.html
+sed -i "s/8\">/8\"\)\'>/g" index.html
+sed -i "s/9\">/9\"\)\'>/g" index.html
+sed -i "s/0\">/0\"\)\'>/g" index.html
