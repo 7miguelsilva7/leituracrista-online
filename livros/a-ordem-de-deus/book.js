02a96ea4a2d6baf6537516c52c435f3acc787697
7miguelsilva7 <7miguelsilva7@gmail.com>
2018-05-14 21:56:45 -0300

    update

--- livros/a-ordem-de-deus/book.js
+++ livros/a-ordem-de-deus/book.js
@@ -591,3 +591,12 @@ function playpen_text(playpen) {
         previousScrollTop = document.scrollingElement.scrollTop;
     }, { passive: true });
 })();
+
+
+var links = document.links;
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+    if (links[i].hostname != window.location.hostname) {
+        links[i].target = 'right';
+    }
+}
+
