59179babba874d68b0d5209f89b5b09fdef41a08
7miguelsilva7 <7miguelsilva7@gmail.com>
2018-12-13 07:14:19 -0300

    update

---
+++ indice/js/select2.js
@@ -0,0 +1,46 @@
+//Select2
+$.getScript('http://cdnjs.cloudflare.com/ajax/libs/select2/3.4.8/select2.min.js',function(){
+           
+  /* dropdown and filter select */
+  var select = $('#select1').select2();
+  var select = $('#select2').select2();
+  var select = $('#select3').select2();
+  var select = $('#select4').select2();
+  var select = $('#select5').select2();
+  var select = $('#select6').select2();
+  var select = $('#select7').select2();
+  var select = $('#select8').select2();
+  var select = $('#select9').select2();
+  var select = $('#select10').select2();
+  var select = $('#select11').select2();
+  var select = $('#select12').select2();
+  var select = $('#select13').select2();
+  var select = $('#select14').select2();
+  var select = $('#select15').select2();
+  var select = $('#select16').select2();
+  var select = $('#select18').select2();
+  var select = $('#select19').select2();
+  var select = $('#select20').select2();
+  var select = $('#select21').select2();
+  var select = $('#select22').select2();
+  var select = $('#select23').select2();
+  var select = $('#select24').select2();
+  var select = $('#select25').select2();
+  var select = $('#select26').select2();
+  var select = $('#select27').select2();
+  var select = $('#select28').select2();
+  var select = $('#select29').select2();
+  var select = $('#select30').select2();
+  
+  
+  
+  
+  /* Select2 plugin as tagpicker */
+  $("#tagPicker").select2({
+    closeOnSelect:false
+  });
+
+}); //script         
+      
+
+$(document).ready(function() {});
---
+++ livros/cartas-aos-evangelistas/comandoEmPastas.sh~
@@ -0,0 +1,47 @@
+#!/bin/bash
+ 
+#livretes ############################################################################################################################
+
+# Diretório a partir do qual estão todas as pastas
+
+(
+echo 'Executando tarefa, aguarde... '
+
+basedir=`pwd` 
+# é possível colocar o link direto,
+# ou copiar o .sh para todas as pastas,
+# o que é necessário nesse exemplo
+
+
+
+# Salva uma lista de diretórios
+cd $basedir
+ls --format single-column >lista.txt 
+# imprime uma lista linear com todos os 
+# diretórios presentes na pasta em que o script está
+
+# Loop para execução do comando
+for subpastas in `cat lista.txt`
+  do
+  cd $basedir/$subpastas/     
+# vai entrar um por um nos diretórios do arquivo 
+# gerado acima, adentrar a pasta em negrito,
+# no caso edit_dir, que está dentro de cada
+# um dos diretórios e concomitantemente...
+
+###### Digite seus comando aqui ########
+ 
+sed -i 's/{#.*}//g' *.md
+
+########################################
+                                                                           
+# ...executar o comando desejado, no caso, phredPhrap
+  done
+
+# Fecha o Script
+exit 0
+
+#echo '   Tarefas finalizadas!!!    '
+sleep 1
+) | zenity --progress --width 300 --height 100 --title "Informação do Sistema" --text "Executando tarefa, aguarde... !!!" --pulsate --auto-close
+
