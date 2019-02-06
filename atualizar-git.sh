72954143e49cb6c4e9c1d912ff0326145434472b
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-02-05 23:52:38 -0300

    update

--- atualizar-git.sh
+++ atualizar-git.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+cp -ur zap /opt/lampp/htdocs/leituracrista-online/
+
 git pull
 #git add --all
 git add --all :/
 git commit -m "update"
 git push
+
+cd /opt/lampp/htdocs/leituracrista-online/
+
+git pull
+#git add --all
+git add --all :/
+git commit -m "update"
+git push
+
+if [ -e /opt/lampp/htdocs/ssh-leituracrista.sh ]; then 
+/opt/lampp/htdocs/ssh-leituracrista.sh;
+
+else 
+chromium-browser https://cpanel.hostinger.com.br/git/index/aid/29500595; 
+fi
