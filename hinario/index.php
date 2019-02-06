22e7bf0150d6e1b1f968a06315ef3c95cd9c73d7
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-02-06 03:42:50 -0300

    update

--- hinario/index.php
+++ hinario/index.php
@@ -165,8 +165,8 @@ border-top:3px dashed #FFF
 
   <div id=\"myDIV\" class=\"footer-mt\">
      <a href=\"#\" class=\"btn\" data-cad=\"@Todos@\" onclick=\"filterSelection('matematica')\"><B >HINOS</B></a><P>
-     <a href=\"#\" class=\"btn\" data-cad=\"@001@\">001</a>
-     <a href=\"#\" class=\"btn\" data-cad=\"@002@\">002</a>
+     <a onclick=\"myFunction()\" href=\"#\" class=\"btn\" data-cad=\"@001@\">001</a>
+     <a onclick=\"myFunction()\" href=\"#\" class=\"btn\" data-cad=\"@002@\">002</a>
      <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D03@\">D03</a>
      <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D04@\">D04</a>
      <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D05@\">D05</a>
