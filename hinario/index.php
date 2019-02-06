3002de5d651438b122137d7d79c003ac23b25942
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-02-06 13:09:28 -0300

    update

--- hinario/index.php
+++ hinario/index.php
@@ -408,7 +408,7 @@ if ($pc<$tp) {
 
 </body>
 
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<!-- <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> -->
 
 <script>
 
