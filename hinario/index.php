869636e2353731893571a5419cb699ec580f841a
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-02-06 03:00:16 -0300

    update

--- hinario/index.php
+++ hinario/index.php
@@ -160,51 +160,7 @@ border-top:3px dashed #FFF
 
 
 <?php
-$total_reg = "2000"; // número de registros por página
-?>
 
-<?php
-// $disciplina	= $_POST ["disciplina"];
-$nivel	= $_POST ["nivel"];	
-require_once '../../edumais/connect.php';
-
-$con = mysql_connect("$host","$user","$pass");
-mysql_query("SET NAMES 'utf8'", $con);
-mysql_query('SET character_set_connection=utf8', $con);
-mysql_query('SET character_set_client=utf8', $con);
-mysql_query('SET character_set_results=utf8', $con);
-$db = mysql_select_db("$db");
-?>
-
-<?php
-
-// $disciplina	= $_POST ["disciplina"];
-$nivel	= $_POST ["nivel"];
-
-
-
-$busca = "SELECT
-p.name as name, p.id as id,
-replace(p.questiontext,'</p><br>','') as questiontext,
-qc.name as category, character_length(p.name) as length,
-substr(p.name,4,3) as descritor,
-substr(p.name,1,2) as disciplina,
-replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(answer,'<p style\=\"display:inline;\">',''),'</br>',''),'</p>',''),'(A)','A)'),'(B)','B)'),'(C)','C)'),'(D)','D)'),'B)','</br>B)'),'C)','</br>C)'),'D)','</br>D)') as answer
-from cs_perguntas p
-inner join cs_respostas_concat r on r.question=p.id
-inner join fhrw_question_categories qc on qc.id=p.category
-where 
-
-substr(qc.name,6,1)=$nivel
-
-group by p.id, r.question
-order by category DESC, name";
-?>
-
-
-<!-- seletor de descritor Fundamental 1 -->
-<?if ($nivel == 1)
-{
   echo "
 
   <div class=\"footer-pt\">
@@ -267,87 +223,6 @@ order by category DESC, name";
   </div>";
 }
   ?>
-
-<?if ($nivel == 2)
-{
-  echo "
-  
-    <div class=\"footer-pt\">
-    <a href=\"#\" class=\"btn\" data-cad=\"@PT@\" onclick=\"filterSelection('portugues')\"><B >PORTUGUÊS</B></a><P>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D01@\">D01</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D02@\">D02</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D03@\">D03</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D04@\">D04</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D05@\">D05</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D06@\">D06</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D07@\">D07</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D08@\">D08</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D09@\">D09</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D10@\">D10</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D11@\">D11</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D12@\">D12</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D13@\">D13</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D14@\">D14</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D15@\">D15</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D16@\">D16</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D17@\">D17</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D18@\">D18</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D19@\">D19</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D20@\">D20</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@@D21@\">D21</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@PT@\">Todos</a>
-          
-    <br><br>
-    </div>
-    
-    
-    <div class=\"footer-mt\">
-    <a href=\"#\" class=\"btn\" data-cad=\"@MT@\" onclick=\"filterSelection('matematica')\"><B >MATEMÁTICA</B></a><P>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D01@\">D01</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D02@\">D02</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D03@\">D03</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D04@\">D04</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D05@\">D05</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D06@\">D06</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D07@\">D07</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D08@\">D08</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D09@\">D09</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D10@\">D10</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D11@\">D11</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D12@\">D12</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D13@\">D13</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D14@\">D14</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D15@\">D15</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D16@\">D16</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D17@\">D17</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D18@\">D18</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D19@\">D19</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D20@\">D20</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D21@\">D21</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D22@\">D22</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D23@\">D23</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D24@\">D24</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D25@\">D25</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D26@\">D26</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D27@\">D27</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D28@\">D28</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D29@\">D29</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D30@\">D30</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D31@\">D31</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D32@\">D32</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D33@\">D33</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D34@\">D34</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D35@\">D35</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D36@\">D36</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@@D37@\">D37</a>
-       <a href=\"#\" class=\"btn\" data-cad=\"@MT@\">Todos</a>
-          
-    <br><br>
-    </div>";
-
-}
-?>
-
 
 <?php
 
