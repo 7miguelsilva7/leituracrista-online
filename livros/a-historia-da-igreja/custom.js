8b645b5628a2a4f84d83e40b0e799393db69ed38
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-02-17 23:00:36 -0300

    update

--- livros/a-historia-da-igreja/custom.js
+++ livros/a-historia-da-igreja/custom.js
@@ -1,5 +1,1 @@
 document.write('<a  target="_self" href="http://leituracrista.com/"><img style="width:35px;height:35px;display:scroll;position:fixed;bottom:12px;right:8px;color:#f00;font-weight:bold;"  src="books.png"/></a>');
-
-
-src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-59d4445a35d0aa43"
-
