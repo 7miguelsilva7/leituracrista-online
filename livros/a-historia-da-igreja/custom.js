f03e8f101216c810f1c31cd4fa1b93ec583528f4
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-06-14 16:47:07 -0300

    update

--- livros/a-historia-da-igreja/custom.js
+++ livros/a-historia-da-igreja/custom.js
@@ -5,9 +5,6 @@
 //     document.body.innerHTML = res;
 // }
 
-document.write('<link href="style.css" rel="stylesheet">')
-document.write('<link rel="manifest" href="manifest.webmanifest">')
-
 document.write('<style>#scroll {overflow-y: auto;}</style>')
 
 if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
