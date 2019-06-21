e754085d418c3a1933e8d25dada5093ee4dcd064
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-06-21 04:17:53 -0300

    update

--- livros/acontecimentos-profeticos/custom.js
+++ livros/acontecimentos-profeticos/custom.js
@@ -1,9 +1,3 @@
-// function linkToMysword() {
-//    //adicionar target blank
-//     var str = document.body.innerHTML;
-//     var res = str.replace(/bibliaonline.com.br\/acf\/rm\//g, "mysword.info\/b?r\=Rom_");
-//     document.body.innerHTML = res;
-// }
 
 document.write('<style>#scroll {overflow-y: auto;}</style>')
 
@@ -62,7 +56,102 @@ alert('Referências em Bíblia Online')
 
 }
 
+// verifica valor de session e determina referencias 
+var session = localStorage.ref;
+if ( session == 1 )
+{
+var str = document.getElementById("content").innerHTML;
+var res = str.replace(/bibliaonline.com.br\/acf\/rm\//g, "mysword.info\/b?r\=Rom_") 
+.replace(/bibliaonline.com.br\/acf\/1co\//g, "mysword.info\/b?r\=1Co_")
+.replace(/bibliaonline.com.br\/acf\/1cr\//g, "mysword.info\/b?r\=1Ch_")
+.replace(/bibliaonline.com.br\/acf\/1pe\//g, "mysword.info\/b?r\=1Pe_")
+.replace(/bibliaonline.com.br\/acf\/1rs\//g, "mysword.info\/b?r\=1Ki_")
+.replace(/bibliaonline.com.br\/acf\/1sm\//g, "mysword.info\/b?r\=1Sa_")
+.replace(/bibliaonline.com.br\/acf\/1co\//g, "mysword.info\/b?r\=1Co_")
+.replace(/bibliaonline.com.br\/acf\/1cr\//g, "mysword.info\/b?r\=1Ch_")
+.replace(/bibliaonline.com.br\/acf\/1jo\//g, "mysword.info\/b?r\=1Jo_")
+.replace(/bibliaonline.com.br\/acf\/1pe\//g, "mysword.info\/b?r\=1Pe_")
+.replace(/bibliaonline.com.br\/acf\/1rs\//g, "mysword.info\/b?r\=1Ki_")
+.replace(/bibliaonline.com.br\/acf\/1sm\//g, "mysword.info\/b?r\=1Sa_")
+.replace(/bibliaonline.com.br\/acf\/1ts\//g, "mysword.info\/b?r\=1Th_")
+.replace(/bibliaonline.com.br\/acf\/1tm\//g, "mysword.info\/b?r\=1Ti_")
+.replace(/bibliaonline.com.br\/acf\/2cr\//g, "mysword.info\/b?r\=2Cr_")
+.replace(/bibliaonline.com.br\/acf\/2co\//g, "mysword.info\/b?r\=2Co_")
+.replace(/bibliaonline.com.br\/acf\/2jo\//g, "mysword.info\/b?r\=2Jo_")
+.replace(/bibliaonline.com.br\/acf\/2pe\//g, "mysword.info\/b?r\=2Pe_")
+.replace(/bibliaonline.com.br\/acf\/2rs\//g, "mysword.info\/b?r\=2Ki_")
+.replace(/bibliaonline.com.br\/acf\/2sm\//g, "mysword.info\/b?r\=2Sa_")
+.replace(/bibliaonline.com.br\/acf\/2ts\//g, "mysword.info\/b?r\=2Th_")
+.replace(/bibliaonline.com.br\/acf\/2tm\//g, "mysword.info\/b?r\=2Ti_")
+.replace(/bibliaonline.com.br\/acf\/3jo\//g, "mysword.info\/b?r\=3Jo_")
+.replace(/bibliaonline.com.br\/acf\/ag\//g, "mysword.info\/b?r\=Hag_")
+.replace(/bibliaonline.com.br\/acf\/am\//g, "mysword.info\/b?r\=Amo_")
+.replace(/bibliaonline.com.br\/acf\/ap\//g, "mysword.info\/b?r\=Rev_")
+.replace(/bibliaonline.com.br\/acf\/atos\//g, "mysword.info\/b?r\=Act_")
+.replace(/bibliaonline.com.br\/acf\/at\//g, "mysword.info\/b?r\=Act_")
+.replace(/bibliaonline.com.br\/acf\/cl\//g, "mysword.info\/b?r\=col_")
+.replace(/bibliaonline.com.br\/acf\/ct\//g, "mysword.info\/b?r\=son_")
+.replace(/bibliaonline.com.br\/acf\/dn\//g, "mysword.info\/b?r\=Dan_")
+.replace(/bibliaonline.com.br\/acf\/dt\//g, "mysword.info\/b?r\=Deu_")
+.replace(/bibliaonline.com.br\/acf\/ec\//g, "mysword.info\/b?r\=Ecc_")
+.replace(/bibliaonline.com.br\/acf\/ed\//g, "mysword.info\/b?r\=Ezr_")
+.replace(/bibliaonline.com.br\/acf\/ef\//g, "mysword.info\/b?r\=Eph_")
+.replace(/bibliaonline.com.br\/acf\/et\//g, "mysword.info\/b?r\=Est_")
+.replace(/bibliaonline.com.br\/acf\/ex\//g, "mysword.info\/b?r\=Exo_")
+.replace(/bibliaonline.com.br\/acf\/ez\//g, "mysword.info\/b?r\=Eze_")
+.replace(/bibliaonline.com.br\/acf\/fp\//g, "mysword.info\/b?r\=Php_")
+.replace(/bibliaonline.com.br\/acf\/fm\//g, "mysword.info\/b?r\=Phm_")
+.replace(/bibliaonline.com.br\/acf\/gl\//g, "mysword.info\/b?r\=Gal_")
+.replace(/bibliaonline.com.br\/acf\/gn\//g, "mysword.info\/b?r\=Gen_")
+.replace(/bibliaonline.com.br\/acf\/hb\//g, "mysword.info\/b?r\=Heb_")
+.replace(/bibliaonline.com.br\/acf\/hc\//g, "mysword.info\/b?r\=Hab_")
+.replace(/bibliaonline.com.br\/acf\/is\//g, "mysword.info\/b?r\=Isa_")
+.replace(/bibliaonline.com.br\/acf\/jd\//g, "mysword.info\/b?r\=Jud_")
+.replace(/bibliaonline.com.br\/acf\/jl\//g, "mysword.info\/b?r\=Joe_")
+.replace(/bibliaonline.com.br\/acf\/jn\//g, "mysword.info\/b?r\=Jon_")
+.replace(/bibliaonline.com.br\/acf\/jo\//g, "mysword.info\/b?r\=Joh_")
+.replace(/bibliaonline.com.br\/acf\/jó\//g, "mysword.info\/b?r\=Job_")
+.replace(/bibliaonline.com.br\/acf\/j%C3%B3\//g, "mysword.info\/b?r\=Job_")
+.replace(/bibliaonline.com.br\/acf\/J%C3%B3\//g, "mysword.info\/b?r\=Job_")
+.replace(/bibliaonline.com.br\/acf\/J%C3%93\//g, "mysword.info\/b?r\=Job_")
+.replace(/bibliaonline.com.br\/acf\/jr\//g, "mysword.info\/b?r\=Jer_")
+.replace(/bibliaonline.com.br\/acf\/js\//g, "mysword.info\/b?r\=Jos_")
+.replace(/bibliaonline.com.br\/acf\/jz\//g, "mysword.info\/b?r\=Jdg_")
+.replace(/bibliaonline.com.br\/acf\/lc\//g, "mysword.info\/b?r\=Luk_")
+.replace(/bibliaonline.com.br\/acf\/lm\//g, "mysword.info\/b?r\=Lam_")
+.replace(/bibliaonline.com.br\/acf\/lv\//g, "mysword.info\/b?r\=Lev_")
+.replace(/bibliaonline.com.br\/acf\/mc\//g, "mysword.info\/b?r\=Mar_")
+.replace(/bibliaonline.com.br\/acf\/ml\//g, "mysword.info\/b?r\=Mal_")
+.replace(/bibliaonline.com.br\/acf\/mq\//g, "mysword.info\/b?r\=Mic_")
+.replace(/bibliaonline.com.br\/acf\/mt\//g, "mysword.info\/b?r\=Mat_")
+.replace(/bibliaonline.com.br\/acf\/na\//g, "mysword.info\/b?r\=Nah_")
+.replace(/bibliaonline.com.br\/acf\/ne\//g, "mysword.info\/b?r\=Neh_")
+.replace(/bibliaonline.com.br\/acf\/nm\//g, "mysword.info\/b?r\=Num_")
+.replace(/bibliaonline.com.br\/acf\/ob\//g, "mysword.info\/b?r\=Oba_")
+.replace(/bibliaonline.com.br\/acf\/os\//g, "mysword.info\/b?r\=Hos_")
+.replace(/bibliaonline.com.br\/acf\/pv\//g, "mysword.info\/b?r\=Pro_")
+.replace(/bibliaonline.com.br\/acf\/rm\//g, "mysword.info\/b?r\=Rom_")
+.replace(/bibliaonline.com.br\/acf\/rt\//g, "mysword.info\/b?r\=Rut_")
+.replace(/bibliaonline.com.br\/acf\/sf\//g, "mysword.info\/b?r\=Zep_")
+.replace(/bibliaonline.com.br\/acf\/sl\//g, "mysword.info\/b?r\=Psa_")
+.replace(/bibliaonline.com.br\/acf\/tg\//g, "mysword.info\/b?r\=Jas_")
+.replace(/bibliaonline.com.br\/acf\/tt\//g, "mysword.info\/b?r\=Tit_")
+.replace(/bibliaonline.com.br\/acf\/zc\//g, "mysword.info\/b?r\=Zec_")
+.replace(/1\//g,"1\:")
+.replace(/2\//g,"2\:")
+.replace(/3\//g,"3\:")
+.replace(/4\//g,"4\:")
+.replace(/5\//g,"5\:")
+.replace(/6\//g,"6\:")
+.replace(/7\//g,"7\:")
+.replace(/8\//g,"8\:")
+.replace(/9\//g,"9\:")
+.replace(/0\//g,"0\:")
+;
+document.getElementById("content").innerHTML = res; 
 
+}else{
+}
 
 function home()
 {
@@ -165,109 +254,12 @@ if(localStorage.hasOwnProperty(positionScrollKey)){
 // log no console do  google
 // console.log(bookNameKey[0].innerHTML)
 // console.log(localStorage.getItem(bookNameKey[0].innerHTML))
-// console.log(positionScrollKey)
-// console.log(localStorage.getItem(positionScrollKey));
+console.log(positionScrollKey)
+console.log(localStorage.getItem(positionScrollKey));
 
-//     var str6 = document.getElementsByTagName("head")[0].innerHTML;
-//     var res6 = str6
-//     .replace(/<link rel="stylesheet" href="book.css">/g, '<link rel="stylesheet" href="book.css">\n<link href="/manifest/style.css" rel="stylesheet">\n<link rel="manifest" href="manifest.webmanifest"></link>')
-//     document.getElementsByTagName("head")[0].innerHTML = res6;
-//     teste=document.getElementsByTagName("head")[0].innerHTML = res6;
-//     console.log(teste)
-
-    // verifica valor de session e determina referencias 
-var session = localStorage.ref;
-if ( session == 1 )
-{
-var str = document.getElementById("content").innerHTML;
-var res = str.replace(/bibliaonline.com.br\/acf\/rm\//g, "mysword.info\/b?r\=Rom_") 
-.replace(/bibliaonline.com.br\/acf\/1co\//g, "mysword.info\/b?r\=1Co_")
-.replace(/bibliaonline.com.br\/acf\/1cr\//g, "mysword.info\/b?r\=1Ch_")
-.replace(/bibliaonline.com.br\/acf\/1pe\//g, "mysword.info\/b?r\=1Pe_")
-.replace(/bibliaonline.com.br\/acf\/1rs\//g, "mysword.info\/b?r\=1Ki_")
-.replace(/bibliaonline.com.br\/acf\/1sm\//g, "mysword.info\/b?r\=1Sa_")
-.replace(/bibliaonline.com.br\/acf\/1co\//g, "mysword.info\/b?r\=1Co_")
-.replace(/bibliaonline.com.br\/acf\/1cr\//g, "mysword.info\/b?r\=1Ch_")
-.replace(/bibliaonline.com.br\/acf\/1jo\//g, "mysword.info\/b?r\=1Jo_")
-.replace(/bibliaonline.com.br\/acf\/1pe\//g, "mysword.info\/b?r\=1Pe_")
-.replace(/bibliaonline.com.br\/acf\/1rs\//g, "mysword.info\/b?r\=1Ki_")
-.replace(/bibliaonline.com.br\/acf\/1sm\//g, "mysword.info\/b?r\=1Sa_")
-.replace(/bibliaonline.com.br\/acf\/1ts\//g, "mysword.info\/b?r\=1Th_")
-.replace(/bibliaonline.com.br\/acf\/1tm\//g, "mysword.info\/b?r\=1Ti_")
-.replace(/bibliaonline.com.br\/acf\/2cr\//g, "mysword.info\/b?r\=2Cr_")
-.replace(/bibliaonline.com.br\/acf\/2co\//g, "mysword.info\/b?r\=2Co_")
-.replace(/bibliaonline.com.br\/acf\/2jo\//g, "mysword.info\/b?r\=2Jo_")
-.replace(/bibliaonline.com.br\/acf\/2pe\//g, "mysword.info\/b?r\=2Pe_")
-.replace(/bibliaonline.com.br\/acf\/2rs\//g, "mysword.info\/b?r\=2Ki_")
-.replace(/bibliaonline.com.br\/acf\/2sm\//g, "mysword.info\/b?r\=2Sa_")
-.replace(/bibliaonline.com.br\/acf\/2ts\//g, "mysword.info\/b?r\=2Th_")
-.replace(/bibliaonline.com.br\/acf\/2tm\//g, "mysword.info\/b?r\=2Ti_")
-.replace(/bibliaonline.com.br\/acf\/3jo\//g, "mysword.info\/b?r\=3Jo_")
-.replace(/bibliaonline.com.br\/acf\/ag\//g, "mysword.info\/b?r\=Hag_")
-.replace(/bibliaonline.com.br\/acf\/am\//g, "mysword.info\/b?r\=Amo_")
-.replace(/bibliaonline.com.br\/acf\/ap\//g, "mysword.info\/b?r\=Rev_")
-.replace(/bibliaonline.com.br\/acf\/atos\//g, "mysword.info\/b?r\=Act_")
-.replace(/bibliaonline.com.br\/acf\/at\//g, "mysword.info\/b?r\=Act_")
-.replace(/bibliaonline.com.br\/acf\/cl\//g, "mysword.info\/b?r\=col_")
-.replace(/bibliaonline.com.br\/acf\/ct\//g, "mysword.info\/b?r\=son_")
-.replace(/bibliaonline.com.br\/acf\/dn\//g, "mysword.info\/b?r\=Dan_")
-.replace(/bibliaonline.com.br\/acf\/dt\//g, "mysword.info\/b?r\=Deu_")
-.replace(/bibliaonline.com.br\/acf\/ec\//g, "mysword.info\/b?r\=Ecc_")
-.replace(/bibliaonline.com.br\/acf\/ed\//g, "mysword.info\/b?r\=Ezr_")
-.replace(/bibliaonline.com.br\/acf\/ef\//g, "mysword.info\/b?r\=Eph_")
-.replace(/bibliaonline.com.br\/acf\/et\//g, "mysword.info\/b?r\=Est_")
-.replace(/bibliaonline.com.br\/acf\/ex\//g, "mysword.info\/b?r\=Exo_")
-.replace(/bibliaonline.com.br\/acf\/ez\//g, "mysword.info\/b?r\=Eze_")
-.replace(/bibliaonline.com.br\/acf\/fp\//g, "mysword.info\/b?r\=Php_")
-.replace(/bibliaonline.com.br\/acf\/fm\//g, "mysword.info\/b?r\=Phm_")
-.replace(/bibliaonline.com.br\/acf\/gl\//g, "mysword.info\/b?r\=Gal_")
-.replace(/bibliaonline.com.br\/acf\/gn\//g, "mysword.info\/b?r\=Gen_")
-.replace(/bibliaonline.com.br\/acf\/hb\//g, "mysword.info\/b?r\=Heb_")
-.replace(/bibliaonline.com.br\/acf\/hc\//g, "mysword.info\/b?r\=Hab_")
-.replace(/bibliaonline.com.br\/acf\/is\//g, "mysword.info\/b?r\=Isa_")
-.replace(/bibliaonline.com.br\/acf\/jd\//g, "mysword.info\/b?r\=Jud_")
-.replace(/bibliaonline.com.br\/acf\/jl\//g, "mysword.info\/b?r\=Joe_")
-.replace(/bibliaonline.com.br\/acf\/jn\//g, "mysword.info\/b?r\=Jon_")
-.replace(/bibliaonline.com.br\/acf\/jo\//g, "mysword.info\/b?r\=Joh_")
-.replace(/bibliaonline.com.br\/acf\/jó\//g, "mysword.info\/b?r\=Job_")
-.replace(/bibliaonline.com.br\/acf\/j%C3%B3\//g, "mysword.info\/b?r\=Job_")
-.replace(/bibliaonline.com.br\/acf\/J%C3%B3\//g, "mysword.info\/b?r\=Job_")
-.replace(/bibliaonline.com.br\/acf\/J%C3%93\//g, "mysword.info\/b?r\=Job_")
-.replace(/bibliaonline.com.br\/acf\/jr\//g, "mysword.info\/b?r\=Jer_")
-.replace(/bibliaonline.com.br\/acf\/js\//g, "mysword.info\/b?r\=Jos_")
-.replace(/bibliaonline.com.br\/acf\/jz\//g, "mysword.info\/b?r\=Jdg_")
-.replace(/bibliaonline.com.br\/acf\/lc\//g, "mysword.info\/b?r\=Luk_")
-.replace(/bibliaonline.com.br\/acf\/lm\//g, "mysword.info\/b?r\=Lam_")
-.replace(/bibliaonline.com.br\/acf\/lv\//g, "mysword.info\/b?r\=Lev_")
-.replace(/bibliaonline.com.br\/acf\/mc\//g, "mysword.info\/b?r\=Mar_")
-.replace(/bibliaonline.com.br\/acf\/ml\//g, "mysword.info\/b?r\=Mal_")
-.replace(/bibliaonline.com.br\/acf\/mq\//g, "mysword.info\/b?r\=Mic_")
-.replace(/bibliaonline.com.br\/acf\/mt\//g, "mysword.info\/b?r\=Mat_")
-.replace(/bibliaonline.com.br\/acf\/na\//g, "mysword.info\/b?r\=Nah_")
-.replace(/bibliaonline.com.br\/acf\/ne\//g, "mysword.info\/b?r\=Neh_")
-.replace(/bibliaonline.com.br\/acf\/nm\//g, "mysword.info\/b?r\=Num_")
-.replace(/bibliaonline.com.br\/acf\/ob\//g, "mysword.info\/b?r\=Oba_")
-.replace(/bibliaonline.com.br\/acf\/os\//g, "mysword.info\/b?r\=Hos_")
-.replace(/bibliaonline.com.br\/acf\/pv\//g, "mysword.info\/b?r\=Pro_")
-.replace(/bibliaonline.com.br\/acf\/rm\//g, "mysword.info\/b?r\=Rom_")
-.replace(/bibliaonline.com.br\/acf\/rt\//g, "mysword.info\/b?r\=Rut_")
-.replace(/bibliaonline.com.br\/acf\/sf\//g, "mysword.info\/b?r\=Zep_")
-.replace(/bibliaonline.com.br\/acf\/sl\//g, "mysword.info\/b?r\=Psa_")
-.replace(/bibliaonline.com.br\/acf\/tg\//g, "mysword.info\/b?r\=Jas_")
-.replace(/bibliaonline.com.br\/acf\/tt\//g, "mysword.info\/b?r\=Tit_")
-.replace(/bibliaonline.com.br\/acf\/zc\//g, "mysword.info\/b?r\=Zec_")
-.replace(/1\//g,"1\:")
-.replace(/2\//g,"2\:")
-.replace(/3\//g,"3\:")
-.replace(/4\//g,"4\:")
-.replace(/5\//g,"5\:")
-.replace(/6\//g,"6\:")
-.replace(/7\//g,"7\:")
-.replace(/8\//g,"8\:")
-.replace(/9\//g,"9\:")
-.replace(/0\//g,"0\:")
-;
-document.getElementById("content").innerHTML = res; 
-
-}else{
-}
+    var str6 = document.getElementsByTagName("head")[0].innerHTML;
+    var res6 = str6
+    .replace(/<link rel="stylesheet" href="book.css">/g, '<link rel="stylesheet" href="book.css">\n<link href="/manifest/style.css" rel="stylesheet">\n<link rel="manifest" href="manifest.webmanifest"></link>')
+    document.getElementsByTagName("head")[0].innerHTML = res6;
+    teste=document.getElementsByTagName("head")[0].innerHTML = res6;
+    console.log(teste)
