d4162ee1a7dc2ae9bc714346221abcc2d850b5d7
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-06-21 14:43:56 -0300

    update

--- js/sw.js
+++ js/sw.js
@@ -5,13 +5,6 @@ window.addEventListener('beforeinstallprompt', (e) => {
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
-});
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
   // Update UI notify the user they can add to home screen
   btnAdd.style.display = 'block';
 });
