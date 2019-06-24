e924f34b024289ade4d84832c6f9b2c3ff5c39c8
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-06-24 17:30:03 -0300

    update

--- livros/a-obra-do-evangelho/custom.js
+++ livros/a-obra-do-evangelho/custom.js
@@ -275,17 +275,20 @@ if(localStorage.hasOwnProperty(positionScrollKey)){
 console.log(positionScrollKey)
 console.log(localStorage.getItem(positionScrollKey));
 
-var strChapter = document.getElementsByClassName("nav-chapters next");
-var resChapter = (strChapter[0])
-.replace(/1\:/g,"1\/")
-.replace(/2\:/g,"2\/")
-.replace(/3\:/g,"3\/")
-.replace(/4\:/g,"4\/")
-.replace(/5\:/g,"5\/")
-.replace(/6\:/g,"6\/")
-.replace(/7\:/g,"7\/")
-.replace(/8\:/g,"8\/")
-.replace(/9\:/g,"9\/")
-.replace(/0\:/g,"0\/")
-;
-console.log(resChapter)
+// var strChapter = document.getElementsByClassName("nav-chapters next");
+// var resChapter = (strChapter[0])
+// .replace(/1\:/g,"1\/")
+// .replace(/2\:/g,"2\/")
+// .replace(/3\:/g,"3\/")
+// .replace(/4\:/g,"4\/")
+// .replace(/5\:/g,"5\/")
+// .replace(/6\:/g,"6\/")
+// .replace(/7\:/g,"7\/")
+// .replace(/8\:/g,"8\/")
+// .replace(/9\:/g,"9\/")
+// .replace(/0\:/g,"0\/")
+// ;
+// console.log(resChapter)
+
+var locationBook = (document.getElementsByClassName("nav-chapters next"));
+alert(locationBook)
