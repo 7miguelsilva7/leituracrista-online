6cce445da68c999699c130c6537469bfbf480fbf
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-06-24 17:16:24 -0300

    update

--- livros/a-obra-do-evangelho/custom.js
+++ livros/a-obra-do-evangelho/custom.js
@@ -275,8 +275,8 @@ if(localStorage.hasOwnProperty(positionScrollKey)){
 console.log(positionScrollKey)
 console.log(localStorage.getItem(positionScrollKey));
 
-var strChapeter = (document.getElementsByClassName("nav-chapters next")[0]);
-var resChapeter = strChapeter
+var strChapeter = document.getElementsByClassName("nav-chapters next");
+var resChapeter = (strChapeter[0])
 .replace(/1\:/g,"1\/")
 .replace(/2\:/g,"2\/")
 .replace(/3\:/g,"3\/")
