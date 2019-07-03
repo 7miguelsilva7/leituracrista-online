01e1e640b22b934ef7af677c798860e1b843207f
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-07-03 17:27:47 -0300

    update

---
+++ app/book/CONV-para-MAISCULO-Inicial.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+sed -i 's/ gn / Gn /g' *.md
+sed -i 's/ ex / Ex /g' *.md
+sed -i 's/ lv / Lv /g' *.md
+sed -i 's/ nm / Nm /g' *.md
+sed -i 's/ dt / Dt /g' *.md
+sed -i 's/ js / Js /g' *.md
+sed -i 's/ jz / Jz /g' *.md
+sed -i 's/ rt / Rt /g' *.md
+sed -i 's/ 1sm / 1 Sm /g' *.md
+sed -i 's/ 2sm / 2 Sm /g' *.md
+sed -i 's/ 1rs / 1 Rs /g' *.md
+sed -i 's/ 2rs / 2 Rs /g' *.md
+sed -i 's/ 1cr / 1 Cr /g' *.md
+sed -i 's/ 2cr / 2 Cr /g' *.md
+sed -i 's/ ed / Ed /g' *.md
+sed -i 's/ ne / Ne /g' *.md
+sed -i 's/ et / Et /g' *.md
+sed -i 's/ jó / Jó /g' *.md
+sed -i 's/ sl / Sl /g' *.md
+sed -i 's/ pv / Pv /g' *.md
+sed -i 's/ ec / Ec /g' *.md
+sed -i 's/ ct / Ct /g' *.md
+sed -i 's/ is / Is /g' *.md
+sed -i 's/ jr / Jr /g' *.md
+sed -i 's/ lm / Lm /g' *.md
+sed -i 's/ ez / Ez /g' *.md
+sed -i 's/ dn / Dn /g' *.md
+sed -i 's/ jl / Jl /g' *.md
+sed -i 's/ am / Am /g' *.md
+sed -i 's/ ob / Ob /g' *.md
+sed -i 's/ jn / Jn /g' *.md
+sed -i 's/ mq / Mq /g' *.md
+sed -i 's/ na / Na /g' *.md
+sed -i 's/ hc / Hc /g' *.md
+sed -i 's/ sf / Sf /g' *.md
+sed -i 's/ ag / Ag /g' *.md
+sed -i 's/ zc / Zc /g' *.md
+sed -i 's/ ml / Ml /g' *.md
+sed -i 's/ mt / Mt /g' *.md
+sed -i 's/ mc / Mc /g' *.md
+sed -i 's/ lc / Lc /g' *.md
+sed -i 's/ jo / Jo /g' *.md
+sed -i 's/ atos / Atos /g' *.md
+sed -i 's/ rm / Rm /g' *.md
+sed -i 's/ 1co / 1 Co /g' *.md
+sed -i 's/ 2co / 2 Co /g' *.md
+sed -i 's/ gl / Gl /g' *.md
+sed -i 's/ ef / Ef /g' *.md
+sed -i 's/ fl / Fl /g' *.md
+sed -i 's/ cl / Cl /g' *.md
+sed -i 's/ 1ts / 1Ts /g' *.md
+sed -i 's/ 2ts / 2Ts /g' *.md
+sed -i 's/ 1tm / 1Tm /g' *.md
+sed -i 's/ 2tm / 2Tm /g' *.md
+sed -i 's/ tt / Tt /g' *.md
+sed -i 's/ fm / Fm /g' *.md
+sed -i 's/ hb / Hb /g' *.md
+sed -i 's/ tg / Tg /g' *.md
+sed -i 's/ 1pe / 1 Pe /g' *.md
+sed -i 's/ 2pe / 2 Pe /g' *.md
+sed -i 's/ 1jo / 1 Jo /g' *.md
+sed -i 's/ 2jo / 2 Jo /g' *.md
+sed -i 's/ 3jo / 3 Jo /g' *.md
+sed -i 's/ jd / Jd /g' *.md
+sed -i 's/ ap / Ap /g' *.md
+
+## entre Colchete
+
+sed -i 's/\[gn/\[Gn/g' *.md
+sed -i 's/\[ex/\[Ex/g' *.md
+sed -i 's/\[lv/\[Lv/g' *.md
+sed -i 's/\[nm/\[Nm/g' *.md
+sed -i 's/\[dt/\[Dt/g' *.md
+sed -i 's/\[js/\[Js/g' *.md
+sed -i 's/\[jz/\[Jz/g' *.md
+sed -i 's/\[rt/\[Rt/g' *.md
+sed -i 's/\[1sm/\[1 Sm/g' *.md
+sed -i 's/\[2sm/\[2 Sm/g' *.md
+sed -i 's/\[1rs/\[1 Rs/g' *.md
+sed -i 's/\[2rs/\[2 Rs/g' *.md
+sed -i 's/\[1cr/\[1 Cr/g' *.md
+sed -i 's/\[2cr/\[2 Cr/g' *.md
+sed -i 's/\[ed/\[Ed/g' *.md
+sed -i 's/\[ne/\[Ne/g' *.md
+sed -i 's/\[et/\[Et/g' *.md
+sed -i 's/\[jó/\[Jó/g' *.md
+sed -i 's/\[sl/\[Sl/g' *.md
+sed -i 's/\[pv/\[Pv/g' *.md
+sed -i 's/\[ec/\[Ec/g' *.md
+sed -i 's/\[ct/\[Ct/g' *.md
+sed -i 's/\[is/\[Is/g' *.md
+sed -i 's/\[jr/\[Jr/g' *.md
+sed -i 's/\[lm/\[Lm/g' *.md
+sed -i 's/\[ez/\[Ez/g' *.md
+sed -i 's/\[dn/\[Dn/g' *.md
+sed -i 's/\[os/\[Os/g' *.md
+sed -i 's/\[jl/\[Jl/g' *.md
+sed -i 's/\[am/\[Am/g' *.md
+sed -i 's/\[ob/\[Ob/g' *.md
+sed -i 's/\[jn/\[Jn/g' *.md
+sed -i 's/\[mq/\[Mq/g' *.md
+sed -i 's/\[na/\[Na/g' *.md
+sed -i 's/\[hc/\[Hc/g' *.md
+sed -i 's/\[sf/\[Sf/g' *.md
+sed -i 's/\[ag/\[Ag/g' *.md
+sed -i 's/\[zc/\[Zc/g' *.md
+sed -i 's/\[ml/\[Ml/g' *.md
+sed -i 's/\[mt/\[Mt/g' *.md
+sed -i 's/\[mc/\[Mc/g' *.md
+sed -i 's/\[lc/\[Lc/g' *.md
+sed -i 's/\[jo/\[Jo/g' *.md
+sed -i 's/\[atos/\[Atos/g' *.md
+sed -i 's/\[at/\[Atos/g' *.md
+sed -i 's/\[rm/\[Rm/g' *.md
+sed -i 's/\[1co/\[1 Co /g' *.md
+sed -i 's/\[2co/\[2 Co /g' *.md
+sed -i 's/\[gl/\[Gl/g' *.md
+sed -i 's/\[ef/\[Ef/g' *.md
+sed -i 's/\[fl/\[Fl/g' *.md
+sed -i 's/\[cl/\[Cl/g' *.md
+sed -i 's/\[1ts/\[1Ts /g' *.md
+sed -i 's/\[2ts/\[2Ts /g' *.md
+sed -i 's/\[1tm/\[1Tm /g' *.md
+sed -i 's/\[2tm/\[2Tm /g' *.md
+sed -i 's/\[tt/\[Tt/g' *.md
+sed -i 's/\[fm/\[Fm/g' *.md
+sed -i 's/\[hb/\[Hb/g' *.md
+sed -i 's/\[tg/\[Tg/g' *.md
+sed -i 's/\[1pe/\[1 Pe/g' *.md
+sed -i 's/\[2pe/\[2 Pe/g' *.md
+sed -i 's/\[1jo/\[1 Jo/g' *.md
+sed -i 's/\[2jo/\[2 Jo/g' *.md
+sed -i 's/\[3jo/\[3 Jo/g' *.md
+sed -i 's/\[jd/\[Jd/g' *.md
+sed -i 's/\[ap/\[Ap/g' *.md
+
+firefox localhost:1313
+
