7c320a1795d217bd7f071968ef0eb3d7dc7df070
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-07-04 00:54:14 -0300

    update

--- app/sw.js
+++ app/sw.js
@@ -1,22 +1,25 @@
-const version = "0.1";
-const cacheName = `leituraCristaApp-${version}`;
-
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll([
-        'index.html',
-        '*',
-      ]);
+ e.waitUntil(
+   caches.open('leituraCristaApp').then(function(cache) {
+     return cache.addAll([
+       '/pwa-examples/a2hs/',
+       '/pwa-examples/a2hs/index.html',
+       '/pwa-examples/a2hs/index.js',
+       '/pwa-examples/a2hs/style.css',
+       '/pwa-examples/a2hs/images/fox1.jpg',
+       '/pwa-examples/a2hs/images/fox2.jpg',
+       '/pwa-examples/a2hs/images/fox3.jpg',
+       '/pwa-examples/a2hs/images/fox4.jpg'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
- });
- 
- self.addEventListener('fetch', function(e) {
-   console.log(e.request.url);
-   e.respondWith(
-     caches.match(e.request).then(function(response) {
-       return response || fetch(e.request);
-     })
-   );
- });
+});
