42e9604a79c3f9dc6a0e68e48c7b70f3188de30a
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-07-06 11:37:04 -0300

    update

--- app/js/sw.js
+++ app/js/sw.js
@@ -1,5 +1,7 @@
-var cache_name = 'leituraCristaApp';
-var cached_urls = [
+ self.addEventListener('install', function(e) {
+        e.waitUntil(
+           caches.open('default-cache').then(function(cache) {
+              return cache.addAll([
   '/app/',
   '/app/index.html',
   '/app/index.xml',
@@ -10,7 +12,6 @@ var cached_urls = [
   '/app/js/custom.js',
   '/app/404.html',
 '/app/acontecimentos-profeticos/index.html',
-'/app/acontecimentos-proféticos/index.html',
 '/app/a-mulher---seu-lugar-nas-escrituras/index.html',
 '/app/a-obra-do-evangelho/index.html',
 '/app/a-oracao-e-as-reunioes-de-oracao/index.html',
@@ -22,53 +23,20 @@ var cached_urls = [
 '/app/luz-para-as-almas-ansiosas/index.html',
 '/app/o-caminho-de-deus-para-o-descanso-poder-e-consagracao/index.html',
 '/app/os-desapontamentos-da-vida/index.html',
+ ])
+        }).then(function() {
+            return self.skipWaiting();
+        }));
+    });
 
-];
+    self.addEventListener('activate', function(e) {
+        e.waitUntil(self.clients.claim());
+    });
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(cache_name)
-    .then(function(cache) {
-      return cache.addAll(cached_urls);
-    })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName.startsWith('leituraCristaApp') && staticCacheName !== cacheName) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-    console.log('Fetch event for ', event.request.url);
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        if (response) {
-          console.log('Found ', event.request.url, ' in cache');
-          return response;
-        }
-        console.log('Network request for ', event.request.url);
-        return fetch(event.request).then(function(response) {
-          if (response.status === 404) {
-            return caches.match('fourohfour.html');
-          }
-          return caches.open(cached_urls).then(function(cache) {
-           cache.put(event.request.url, response.clone());
-            return response;
-          });
-        });
-      }).catch(function(error) {
-        console.log('Error, ', error);
-        return caches.match('/app/index.html');
-      })
-    );
-  });
+    self.addEventListener('fetch', function(e) {
+        e.respondWith(
+            caches.match(e.request).then(function(response) {
+                return response || fetch(e.request);
+            })
+        );
+    });
