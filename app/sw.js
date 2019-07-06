f3ba496f43b68386e669c5c1aee4443bd4f5f357
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-07-06 14:10:42 -0300

    update

--- app/sw.js
+++ app/sw.js
@@ -1,6 +1,6 @@
 // sw.js
 
-var cacheName = 'leituraCristaApp-V1';
+var cacheName = 'leituraCristaAPP-V1';
 
 var filesToCache = [
   '/app/',
@@ -23,52 +23,59 @@ var filesToCache = [
 '/app/luz-para-as-almas-ansiosas/index.html',
 '/app/o-caminho-de-deus-para-o-descanso-poder-e-consagracao/index.html',
 '/app/os-desapontamentos-da-vida/index.html',
+
 ];
 
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(cache_name)
-    .then(function(cache) {
-      return cache.addAll(cached_urls);
-    })
-  );
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(function(cache) {
+            console.info('[sw.js] cached all files');
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName.startsWith('leituraCristaApp') && staticCacheName !== cacheName) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
 
 self.addEventListener('fetch', function(event) {
-    console.log('Fetch event for ', event.request.url);
     event.respondWith(
-      caches.match(event.request).then(function(response) {
-        if (response) {
-          console.log('Found ', event.request.url, ' in cache');
-          return response;
-        }
-        console.log('Network request for ', event.request.url);
-        return fetch(event.request).then(function(response) {
-          if (response.status === 404) {
-            return caches.match('fourohfour.html');
-          }
-          return caches.open(cached_urls).then(function(cache) {
-           cache.put(event.request.url, response.clone());
-            return response;
-          });
-        });
-      }).catch(function(error) {
-        console.log('Error, ', error);
-        return caches.match('/app/index.html');
-      })
+        caches.match(event.request)
+        .then(function(response) {
+            if(response){
+                return response
+            }
+            else{
+                // clone request stream
+                // as stream once consumed, can not be used again
+                var reqCopy = event.request.clone();
+                
+                return fetch(reqCopy, {credentials: 'include'}) // reqCopy stream consumed
+                .then(function(response) {
+                    // bad response
+                    // response.type !== 'basic' means third party origin request
+                    if(!response || response.status !== 200 || response.type !== 'basic') {
+                        return response; // response stream consumed
+                    }
+
+                    // clone response stream
+                    // as stream once consumed, can not be used again
+                    var resCopy = response.clone();
+
+
+                    // ================== IN BACKGROUND ===================== //
+
+                    // add response to cache and return response
+                    caches.open(cacheName)
+                    .then(function(cache) {
+                        return cache.put(reqCopy, resCopy); // reqCopy, resCopy streams consumed
+                    });
+
+                    // ====================================================== //
+
+
+                    return response; // response stream consumed
+                })
+            }
+        })
     );
-  });
+});
