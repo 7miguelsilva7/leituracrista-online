11e6b3204ed9edba5b5c379272cdeece445f3b09
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-07-08 20:32:22 -0300

    update

--- app/js/custom.js
+++ app/js/custom.js
@@ -155,7 +155,7 @@ var res = str.replace(/bibliaonline.com.br\/acf\/rm\//g, "mysword.info\/b?r\=Rom
 .replace(/https\:\/\/www.mysword/g,"https\:\/\/mysword")
 .replace(/http\:\/\/www.mysword/g,"https\:\/\/mysword")
 .replace(/http\:/g,"https\:")
-.replace(/href\=(.*.)\">/g,"onclick=\"window.open('href\=$1');return true;\">")
+.replace(/href\=\"(.*.)\">/g,"onclick=\"window.open('$1')\">")
 
 
 ;
