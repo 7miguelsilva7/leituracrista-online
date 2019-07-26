16eb11376f99f9ce2eaa4a153050d73b546632fe
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-07-26 08:41:05 -0300

    update

--- hinario/sw.js
+++ hinario/sw.js
@@ -9,8 +9,6 @@ let CURRENT_CACHES = {
 const OFFLINE_URL = [
   '/hinario/index.html',
   '/hinario/style.css',
-  '/hinario/img/indice.png',
-  '/hinario/img/numero.png',
 ];
 
 self.addEventListener('install', event => {
