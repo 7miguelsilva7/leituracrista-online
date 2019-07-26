9755da678b9237adf850b5b23e5ee88d3c5304b8
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-07-26 09:25:41 -0300

    update

--- hinario/sw.js
+++ hinario/sw.js
@@ -52,17 +52,11 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
 
-  // adicionado por mim
-  console.log('Fetch event for ', event.request.url);
-
-  if (event.request.mode === 'navigate' ||
+   if (event.request.mode === 'navigate' ||
       (event.request.method === 'GET' &&
        event.request.headers.get('accept').includes('text/html'))) {
     event.respondWith(
       fetch(event.request).catch(error => {
-
-        // adicionado por mim
-        console.log('Found ', event.request.url, ' in cache');
 
         return caches.match(OFFLINE_URL);
       })
