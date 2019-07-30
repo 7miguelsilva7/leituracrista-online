9a7d13ef44dbb3b6287a394e56a3209614419fbc
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-07-30 08:23:19 -0300

    update

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-var cacheName = '&&versaoleituraCristaApp-19-07-30-07:56:48&&versao';
+var cacheName = '&&versaoleituraCristaApp-19-07-30-08:10:07&&versao';
 
 self.addEventListener('install', event => {
   event.waitUntil(
