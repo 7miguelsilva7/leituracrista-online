29da01e18557812db7e2afa46e07fe5316358595
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-07-29 21:03:21 -0300

    update

--- app/readinenglish/js/custom.js
+++ app/readinenglish/js/custom.js
@@ -17,39 +17,3 @@ if(localStorage.hasOwnProperty(positionScrollreadinenglishKey)){
   $(window).scrollTop(localStorage.getItem(positionScrollreadinenglishKey));
   target.css('overflow-y', 'auto');
 }
-
-// TEXTO CLICÁVEL
-var criarLink = t => `<a target="_blank" href="https://translate.google.com/m/translate?ie=UTF8&sl=pt-BR&tl=en&q=#en/pt/${t}">${t}</a>`
-
-var criarArrayDeLinks = e => e.textContent.split(' ').filter(i => i !== '' && i !== '\n').map(i => criarLink(i));
-
-function criarLinksEmElementos(pai) {
-  const tags = [];
-
-  if (pai.nodeType == 3 && pai.textContent.trim()) { 
-    tags.push(...criarArrayDeLinks(e));
-  } else {          
-    pai.childNodes
-       .forEach(
-          e => {         
-            if (e.nodeType == 3 && e.textContent.trim()) { 
-              tags.push(...criarArrayDeLinks(e));
-            } else {          
-              tags.push(criarLinksEmElementos(e).outerHTML);
-            }
-        });
-  }
-  
-  if (tags.length) {    
-    pai.innerHTML = tags.join(' ');    
-  }
-
-  return pai;  
-}
-
-function criarTextoClicavel(seletor) {  
-  const div = document.querySelector(seletor);
-  criarLinksEmElementos(div);
-}
-
-criarTextoClicavel('#texto-clicavel');
