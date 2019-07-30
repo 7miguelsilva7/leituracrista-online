34c58a9b032acd15410e7e1680e4ee44dbf3367b
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-07-29 23:13:54 -0300

    update

--- app/readinenglish/sw-readinenglish.js
+++ app/readinenglish/sw-readinenglish.js
@@ -1,2 +1,36 @@
-'/app/readinenglish/historyofsite/index.html',
+var cacheName = '&&versaoreadinenglish19-07-29-23:07:27&&versao';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll([
+        '/app/readinenglish/',
+  '/app/readinenglish/index.html',
+  '/app/readinenglish/css/main.css',
+  '/app/readinenglish/css/normalize.css',
+  '/app/readinenglish/css/typo.css',
+  '/app/readinenglish/js/custom.js',
+  '/app/readinenglish/closeWindow.html',
+  '/app/readinenglish/historyofsite/index.html',
 '/app/readinenglish/ready-for-the-race/index.html',
+]))
+  );
+});
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
