b9b4e5f3de8f2c08b4c22c3959127c3db9d1318e
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-09-04 00:42:27 -0300

    update

--- linktobible/link.php
+++ linktobible/link.php
@@ -68,6 +68,7 @@ document.getElemntById(divText) = res
  	 (function(d, t) { 
  	 	 var g = d.createElement(t), s = d.getElementsByTagName(t)[0]; 
  	 	 g.src = '//api.reftagger.com/v2/RefTagger.js'; 
+ 	 	 g.src = 'RefTagger.js'; 
  	 	 s.parentNode.insertBefore(g, s); 
  	 }(document, 'script')); 
 </script> 
