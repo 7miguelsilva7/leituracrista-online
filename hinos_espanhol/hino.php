09860b419c8a0ee162056a78181e49456fe4865a
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-10-18 16:17:23 -0300

    Update hino.php

--- hinos_espanhol/hino.php
+++ hinos_espanhol/hino.php
@@ -1,8 +1,8 @@
 <html>
     
 <head>
-<meta charset="UTF-8">
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
