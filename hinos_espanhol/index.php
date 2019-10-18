971a470d0938d26e6c42079bf82ccd93ae8de73d
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-10-18 15:31:11 -0300

    Update index.php

--- hinos_espanhol/index.php
+++ hinos_espanhol/index.php
@@ -1,17 +1,13 @@
 <meta charset="UTF-8">
 <?php
-
 // titulo
   require_once 'dbconnect.php';  
-
  $sql = "SELECT hino FROM hinos_espanhol h";  
  $stm = $PDO->prepare($sql);  
  $stm->execute();  
  $dados = $stm->fetchAll(PDO::FETCH_OBJ);  
  foreach($dados as $reg):  
-
-    echo '&nbsp;&nbsp;<a href="/hinos_espanhol/hino.php?hino=' . $reg->hino . '" style="font-size:16px"> ' . $reg->hino . ' </a>&nbsp;&nbsp;';   
+    echo '&nbsp;&nbsp;<a href="/hinos_espanhol/hino.php?hino=' . $reg->hino . '" style="font-size:16px"> ' . str_pad($reg->hino, 3, 0, STR_PAD_LEFT) . '&nbsp;&nbsp;</a>';   
        
-  endforeach;
-
+ endforeach;
   ?> 
