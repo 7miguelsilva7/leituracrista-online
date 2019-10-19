a5055f62d3bb996f7145a7e9a4ca29ded9e94fac
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-10-19 10:17:56 -0300

    update

--- hinos_espanhol/hino.php
+++ hinos_espanhol/hino.php
@@ -1,29 +1,58 @@
-<meta charset="UTF-8">
+<html>
+    
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+
+
+</head>
+
 
 <a href="/hinos_espanhol/"><button>Índice</button></a>
 
 <style>
-div.a {
+.alert-box {
+	padding: 15px;
+    margin-bottom: 20px;
+    border: 1px solid transparent;
+    border-radius: 4px;  
+}
+.success {
+    color: #3c763d;
+    background-color: #dff0d8;
+    border-color: #d6e9c6;
+    display: none;
+}  
+  
+span.a {
   font-size: 15px;
 }
-
-div.b {
+span.b {
   font-size: large;
 }
-
-div.c {
+span.c {
   font-size: 150%;
 }
 </style>
 
+
 <?php
+ require_once 'dbconnect.php';  
 
 // hino get
 $hino = $_GET['hino'];
 
 // titulo
-  require_once 'dbconnect.php';  
-
  $sql = "SELECT h.hino, h.titulo, h.metrica, h.descricao FROM hinos_espanhol h
  inner join estrofes_espanhol e on e.hino=h.hino
  where h.hino = $hino
@@ -33,33 +62,56 @@ $hino = $_GET['hino'];
  $stm->execute();  
  $dados = $stm->fetchAll(PDO::FETCH_OBJ);  
  foreach($dados as $reg):  
-
-  echo '<div class="c" align="center">' . $hino . ' - <b>' . $reg->titulo . '</b></div>';   
-  echo '<div class="b" align="center">' . $reg->metrica . '</b></div>';   
-  echo '<div class="b" align="center">' . $reg->descricao . '</b></div>';   
+  echo '<div align="center"><span class="c" >' . $hino . ' - <b>' . $reg->titulo . '</b></span> </div>';   
+  echo '<div align="center"><span class="b" >' . $reg->metrica . '</b></span></div>';   
+  echo '<div align="center"><span class="b" >' . $reg->descricao . '</b></span></div>';   
        
   endforeach;
-
   ?>
   <br>
-  
+
+  <div style="position:fixed; right:10px; top:10px" class="alert-box success">Copiado!!!</div>
+
   <?
   // hino
   require_once 'dbconnect.php';  
-
- $sql = "SELECT h.hino, linha, refrao FROM hinos_espanhol h
- inner join estrofes_espanhol e on e.hino=h.hino
- where h.hino = $hino
+ $sql = "SELECT `hino`
+ ,`estrofe` as estrofeid
+ ,GROUP_CONCAT(`linha` SEPARATOR '<br>') as estrofe
+ ,`refrao` 
+ FROM `estrofes_espanhol` 
+ where hino = $hino
+ group by estrofe, hino 
  ";  
  $stm = $PDO->prepare($sql);  
  $stm->execute();  
  $dados = $stm->fetchAll(PDO::FETCH_OBJ);  
  foreach($dados as $reg):  
-
     if ($reg->refrao == 1){ echo '<i style="color:blue">' ;}
-    echo '<div class="c" align="center">' . $reg->linha . '</div>';
-    if ($reg->refrao == 1){ echo '</i>' ;}
-       
-  endforeach;
+    echo '<div align="center">' . $reg->estrofeid . '. <span onclick="copyDivToClipboard' . $reg->estrofeid . '()" style="cursor:pointer" class="c DivSuccess" id="divText' . $reg->estrofeid . '"   >' . $reg->estrofe . '</span></div><p>';
+    if ($reg->refrao == 1){ echo '</i>' ;}?>
 
-  ?> 
+    <script>
+    function copyDivToClipboard<?echo $reg->estrofeid?>() {
+                        var range = document.createRange();
+                        range.selectNode(document.getElementById("divText<?echo $reg->estrofeid?>"));
+                        window.getSelection().removeAllRanges(); // clear current selection
+                        window.getSelection().addRange(range); // to select text
+                        document.execCommand("copy");
+                        window.getSelection().removeAllRanges();// to deselect
+    //                     alert('Estrofe Copiada!')
+                        $( "div.success" ).fadeIn( 50 ).delay( 1000 ).fadeOut( 100 );
+    
+          }
+    </script> 
+<?
+ endforeach;
+?> 
+
+
+
+<script>
+// $( ".DivSuccess" ).click(function() {
+// $( "div.success" ).fadeIn( 50 ).delay( 1000 ).fadeOut( 100 );
+// });
+</script>
--- hinos_espanhol/index.php
+++ hinos_espanhol/index.php
@@ -1,17 +1,29 @@
-<meta charset="UTF-8">
-<?php
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<style>
+
+a:link{
+text-decoration: none;
+}
+div
+{
+ font-size: 20px;
+ line-height: 1.9;
+}
+
+</style>
+<div align="left">
+<?php
 // titulo
   require_once 'dbconnect.php';  
-
  $sql = "SELECT hino FROM hinos_espanhol h";  
  $stm = $PDO->prepare($sql);  
  $stm->execute();  
  $dados = $stm->fetchAll(PDO::FETCH_OBJ);  
  foreach($dados as $reg):  
-
-    echo '&nbsp;&nbsp;<a href="/hinos_espanhol/hino.php?hino=' . $reg->hino . '" style="font-size:16px"> ' . $reg->hino . ' </a>&nbsp;&nbsp;';   
+    echo '<a href="/hinos_espanhol/hino.php?hino=' . $reg->hino . '" style="font-size:16px">&nbsp;&nbsp;&nbsp;&nbsp; ' . str_pad($reg->hino, 3, 0, STR_PAD_LEFT) . '&nbsp;&nbsp;&nbsp;&nbsp;</a>';   
        
-  endforeach;
-
+ endforeach;
   ?> 
+  </div >
