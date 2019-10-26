860c09607f33993beea00df36f4b0a150064005a
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-10-26 11:46:38 -0300

    update

--- hinario/sw.js
+++ hinario/sw.js
@@ -1,10 +1,8 @@
 let CURRENT_CACHES = {
   offline: 'offlineHinario-v2'
 };
-const OFFLINE_URL = [
+const OFFLINE_URL = 
        'index.html'
-       
-]
 ;
 
 self.addEventListener('install', event => {
