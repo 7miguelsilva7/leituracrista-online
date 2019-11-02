bdca2a63760207b8b0a684da9a143ae1e42f3365
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-11-01 21:50:51 -0300

    update

--- assembleias/index.php
+++ assembleias/index.php
@@ -1,6 +1,7 @@
 <script src="sorttable.js"></script>
 <script language="JavaScript" type="text/javascript" src="cidades-estados-utf8.js"></script>
 <script src="js/jquery-2.1.1.min.js"></script>
+<script src="select2.min.js"></script>
 
 <!-- select 2 -->
 <link href="print.css" rel="stylesheet" type="text/css" media="print">
@@ -304,4 +305,4 @@ foreach ($arr as &$valuedestino) {
   }
 </script>
 
-<script src="select2.min.js"></script>
+<script src="select2.js"></script>
