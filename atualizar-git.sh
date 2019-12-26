4b7f5188409f634631793f50dc170ea3bf88db30
7miguelsilva7 <7miguelsilva7@gmail.com>
2019-12-25 22:07:11 -0300

    update

--- atualizar-git.sh
+++ atualizar-git.sh
@@ -12,7 +12,7 @@ cp -r manifest /opt/lampp/htdocs/leituracrista-online/
 cp -r linktobible /opt/lampp/htdocs/leituracrista-online/
 cp -r assembleias /opt/lampp/htdocs/leituracrista-online/
 cp -r app/leituracrista/public/* /opt/lampp/htdocs/leituracrista-online/app/
-cp -r app/readinenglish/public* /opt/lampp/htdocs/leituracrista-online/app/readinenglish/
+cp -r app/readinenglish/public/* /opt/lampp/htdocs/leituracrista-online/app/readinenglish/
 
 git pull
 #git add --all
