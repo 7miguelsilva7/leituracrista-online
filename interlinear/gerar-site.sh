dd7173c3adf54aab5ab2a64e2e884a6eb9adcd0c
Miguel Silva <7miguelsilva7@gmail.com>
2020-01-10 19:32:12 -0300

    update

---
+++ interlinear/gerar-site.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+hugo 
+# copia head e foot para juntar ao index
+cp script.head.html script.foot.html docs
+
+# Junta arquivos
+cd docs
+cat script.head.html index.html script.foot.html > index2.html
+mv index2.html index.html
