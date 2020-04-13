97217ccf6a6432f3b5bd86aa5e173ddd0f340e6b
Miguel Silva <7miguelsilva7@gmail.com>
2020-04-13 14:14:32 -0300

    update

--- tests/ajax.php
+++ tests/ajax.php
@@ -1,18 +1,4 @@
 <?
-function file_get_contents_curl($url)
-	{
-		$ch = curl_init();
-	
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	
-		$data = curl_exec($ch);
-		curl_close($ch);
-	
-		return $data;
-	}
 
 $html = file_get_contents('https://www.bibliaonline.com.br/acf/zc/1/18-20');
 $dom = new DOMDocument();
