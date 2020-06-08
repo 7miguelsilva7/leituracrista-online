cfe5b8cf633fb650072803fb57b78e0af60dbd6d
Miguel Silva <7miguelsilva7@gmail.com>
2020-06-08 17:30:54 -0300

    update

--- biblia/text.php
+++ biblia/text.php
@@ -2,7 +2,7 @@
     
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
