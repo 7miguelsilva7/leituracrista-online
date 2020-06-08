257e176e5452447ff5e93ffb217b395efd56cb87
Miguel Silva <7miguelsilva7@gmail.com>
2020-06-08 03:15:29 -0300

    update

--- biblia/cap.php
+++ biblia/cap.php
@@ -15,7 +15,12 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
 
 <style>
-body {margin: 50px}
+body {
+  
+  margin: 50px;
+  font-size: 20px;
+  line-height: 1.9;
+  }
 
 /* css copiar texto */
 .alert-box {
