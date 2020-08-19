742ac68ef37112a579738713bc30379e8b1ce438
Miguel Silva <7miguelsilva7@gmail.com>
2020-08-18 22:36:45 -0300

    update

--- biblia/cap.php
+++ biblia/cap.php
@@ -3,6 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" href="img/bible-icon.jpg">
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
