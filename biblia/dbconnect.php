22b9b485da7c85d202c32b102a228eadb97ac78f
Miguel Silva <7miguelsilva7@gmail.com>
2020-11-17 01:06:14 -0300

    update

--- biblia/dbconnect.php
+++ biblia/dbconnect.php
@@ -2,24 +2,13 @@
 $whitelist = array('127.0.0.1', "::1");
 
 if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
-    // se online
     $host='localhost';
-    $username='u378308740_biblia';
-    $password='gogo1352';
-    $db='u378308740_biblia';
 }else{
-    // Se localmente
-    $host='localhost';
-    $username='root';
-    $password='';
-    $db='biblia';
+    $host='sql112.main-hosting.eu';
 }
 
+$username='u378308740_biblia';
+$password='gogo1352';
+$db='u378308740_biblia';
 $PDO = new PDO( 'mysql:host=' . $host . ';dbname=' . $db, $username, $password);
-
-$mysqli = new mysqli($host, $username, $password, $db);
-if ($mysqli->connect_errno) {
-    echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-
 ?>
