38619f602f271509f4527855b2dda7b6bea2692a
Miguel Silva <7miguelsilva7@gmail.com>
2020-12-11 02:44:57 -0300

    update

--- app/js/refLink.js
+++ app/js/refLink.js
@@ -59,3 +59,32 @@ $("#content").each(function() {
               $(this).attr("href", newUrl); 
   });
   });
+
+// Marcar livros como lido ou não
+var aStatus='Status'
+var bookNameStatus = document.getElementsByTagName("title")[0].innerHTML;
+var livroStatus = bookNameStatus.concat(aStatus)
+
+if (localStorage.getItem(livroStatus) === null) {
+  
+localStorage.setItem(livroStatus, 0)
+}
+
+function setLivroStatus0()
+{
+localStorage.setItem(livroStatus, 0)
+}
+function setLivroStatus1()
+{
+localStorage.setItem(livroStatus, 1)
+}
+
+if (localStorage.getItem(livroStatus) == 0)
+{
+document.write("</div><div align='center'><a href='../' onclick='setLivroStatus1();removeNameBookActived()'>Marcar Livro Como Lido</a></div><br><br>")
+}
+else
+{
+document.write("<div align='center'><a href='../' onclick='setLivroStatus0();removeNameBookActived()'>Desmarcar Livro Como Lido</a></div><br><br")
+}
+//Fim Marcar livros como lido ou não
