e900a95a8a6e39db6ada255c4c81105c080ad82d
Miguel Silva <7miguelsilva7@gmail.com>
2020-12-16 03:30:45 -0300

    update

--- linktobible/refLink.js
+++ linktobible/refLink.js
@@ -248,32 +248,32 @@ if ( session == 1 )
 }); // fim de read function
 
 // Marcar livros como lido ou não
-var aStatus='Status'
-var bookNameStatus = document.getElementsByTagName("title")[0].innerHTML;
-var livroStatus = bookNameStatus.concat(aStatus)
+// var aStatus='Status'
+// var bookNameStatus = document.getElementsByTagName("title")[0].innerHTML;
+// var livroStatus = bookNameStatus.concat(aStatus)
 
-if (localStorage.getItem(livroStatus) === null) {
+// if (localStorage.getItem(livroStatus) === null) {
   
-localStorage.setItem(livroStatus, 0)
-}
+// localStorage.setItem(livroStatus, 0)
+// }
 
-function setLivroStatus0()
-{
-localStorage.setItem(livroStatus, 0)
-}
-function setLivroStatus1()
-{
-localStorage.setItem(livroStatus, 1)
-}
+// function setLivroStatus0()
+// {
+// localStorage.setItem(livroStatus, 0)
+// }
+// function setLivroStatus1()
+// {
+// localStorage.setItem(livroStatus, 1)
+// }
 
-if (localStorage.getItem(livroStatus) == 0)
-{
-document.write("</div><div align='center'><a href='../' onclick='setLivroStatus1();removeNameBookActived()'>Marcar Livro Como Lido</a></div><br><br>")
-}
-else
-{
-document.write("<div align='center'><a href='../' onclick='setLivroStatus0();removeNameBookActived()'>Desmarcar Livro Como Lido</a></div><br><br")
-}
+// if (localStorage.getItem(livroStatus) == 0)
+// {
+// document.write("</div><div align='center'><a href='../' onclick='setLivroStatus1();removeNameBookActived()'>Marcar Livro Como Lido</a></div><br><br>")
+// }
+// else
+// {
+// document.write("<div align='center'><a href='../' onclick='setLivroStatus0();removeNameBookActived()'>Desmarcar Livro Como Lido</a></div><br><br")
+// }
 //Fim Marcar livros como lido ou não
 
 
